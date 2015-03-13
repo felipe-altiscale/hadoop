1f5687882041e4674a70909364c0b238eefa4917
Abin Shahab <ashahab@altiscale.com>
2015-03-13 10:40:32 -0700

    removed findbugs warning

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/DockerContainerExecutor.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/DockerContainerExecutor.java
@@ -166,9 +166,6 @@ public class DockerContainerExecutor extends LinuxContainerExecutor {
         return ExitCode.TERMINATED.getExitCode();
       }
     } catch (IOException e) {
-      if (null == shExec) {
-        return -1;
-      }
       int exitCode = shExec.getExitCode();
       LOG.warn("Exit code from container " + containerId + " is : " + exitCode);
       // 143 (SIGTERM) and 137 (SIGKILL) exit codes means the container was
