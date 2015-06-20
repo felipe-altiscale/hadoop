a020ca1a810822dfa3ab693e265e23a405876515
Yongjun Zhang <yzhang@cloudera.com>
2015-06-19 23:39:13 -0700

    HDFS-8337. Accessing httpfs via webhdfs doesn't work from a jar with kerberos. Contributed by Yongjun Zhang.
    (cherry picked from commit 971dc838ecf8bf55a9bd64128ce2447e4613ea76)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt
    	hadoop-hdfs-httpfs/src/main/java/org/apache/hadoop/fs/http/server/HttpFSAuthenticationFilter.java
    
    The second conflict is not a conflict in the git sense, but the commit refers
    to a constants class added in an earlier 2.8 commit, 5d3a4d5.
    ovy@altiscale inlined the constant for now.

--- hadoop-hdfs-project/hadoop-hdfs-httpfs/src/main/java/org/apache/hadoop/fs/http/server/HttpFSAuthenticationFilter.java
+++ hadoop-hdfs-project/hadoop-hdfs-httpfs/src/main/java/org/apache/hadoop/fs/http/server/HttpFSAuthenticationFilter.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.security.authentication.server.AuthenticationFilter;
 import org.apache.hadoop.security.token.delegation.web.DelegationTokenAuthenticationFilter;
+import org.apache.hadoop.security.token.delegation.web.KerberosDelegationTokenAuthenticationHandler;
 
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
@@ -92,6 +93,11 @@ public class HttpFSAuthenticationFilter
     } catch (IOException ex) {
       throw new RuntimeException("Could not read HttpFS signature secret file: " + signatureSecretFile);
     }
+    setAuthHandlerClass(props);
+    props.setProperty(KerberosDelegationTokenAuthenticationHandler.TOKEN_KIND,
+                      // NB: In the non-cherrypicked future the constant
+                      // below is WebHdfsConstants.WEBHDFS_TOKEN_KIND.
+                      "WEBHDFS delegation");
     return props;
   }
 
