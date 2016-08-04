ded4bae4b8b54c40837ef791d56569edcf282a89
Arpit Agarwal <arp@apache.org>
2016-08-04 10:07:53 -0700

    HADOOP-13467. Shell#getSignalKillCommand should use the bash builtin on Linux. (Arpit Agarwal)

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestShell.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestShell.java
@@ -17,6 +17,8 @@
  */
 package org.apache.hadoop.util;
 
+import org.junit.Assert;
+
 import junit.framework.TestCase;
 
 import java.io.BufferedReader;
@@ -29,6 +31,7 @@ import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 
 import org.apache.hadoop.fs.FileUtil;
+import static org.apache.hadoop.util.Shell.*;
 
 public class TestShell extends TestCase {
 
@@ -150,7 +153,47 @@ public class TestShell extends TestCase {
     System.err.println("after: " + timersAfter);
     assertEquals(timersBefore, timersAfter);
   }
-  
+
+  public void testGetCheckProcessIsAliveCommand() throws Exception {
+    String anyPid = "9999";
+    String[] checkProcessAliveCommand = getCheckProcessIsAliveCommand(
+        anyPid);
+
+    String[] expectedCommand;
+
+    if (Shell.WINDOWS) {
+      expectedCommand =
+          new String[]{getWinUtilsPath(), "task", "isAlive", anyPid };
+    } else if (Shell.isSetsidAvailable) {
+      expectedCommand = new String[] { "bash", "-c", "kill -0 -- -'" +
+            anyPid + "'"};
+    } else {
+      expectedCommand = new String[] {"bash", "-c", "kill -0 '" + anyPid +
+            "'" };
+    }
+    Assert.assertArrayEquals(expectedCommand, checkProcessAliveCommand);
+  }
+
+  public void testGetSignalKillCommand() throws Exception {
+    String anyPid = "9999";
+    int anySignal = 9;
+    String[] checkProcessAliveCommand = getSignalKillCommand(anySignal,
+        anyPid);
+
+    String[] expectedCommand;
+
+    if (Shell.WINDOWS) {
+      expectedCommand =
+          new String[]{getWinUtilsPath(), "task", "kill", anyPid };
+    } else if (Shell.isSetsidAvailable) {
+      expectedCommand = new String[] { "bash", "-c", "kill -9 -- -'" + anyPid +
+            "'"};
+    } else {
+      expectedCommand = new String[]{ "bash", "-c", "kill -9 '" + anyPid +
+            "'"};
+    }
+    Assert.assertArrayEquals(expectedCommand, checkProcessAliveCommand);
+  }
 
   private void testInterval(long interval) throws IOException {
     Command command = new Command(interval);
@@ -166,7 +209,6 @@ public class TestShell extends TestCase {
     }
   }
 
-  @Test
   public void testBashQuote() {
     assertEquals("'foobar'", Shell.bashQuote("foobar"));
     assertEquals("'foo'\\''bar'", Shell.bashQuote("foo'bar"));
